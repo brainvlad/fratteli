2c072bbcce68618217e89af3f47986e62d388cf2
Dmitry Yegorov <dmitry.yegorov@effective-soft.com>
2022-05-30 12:44:33 +0300

    added template

--- app/account/save-user-level.php
+++ app/account/save-user-level.php
@@ -1,6 +1,15 @@
 <?php
-
+session_start();
 try {
+
+
+    header("Content-Type: application/json;");
+    $user_id = $_SESSION['user']['id'];
+
+    $json = file_get_contents('php://input');
+    $req = json_decode($json, true);
+
+    $user_level = $req["user_level"];
 
     // implement setting data to db
 
